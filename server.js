4323486812ca59ded885015095ac5d50ec66878b
lilika67 <l.kayitesi@alustudent.com>
2023-12-19 09:09:54 +0200

    new changes

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-require('dotenv').config();
-const express = require('express');
+// require('dotenv').config();
+// const express = require('express');
+
+// const  allRoutes=require('./routes');
+// const {connectDb}=require('./db/databaseconnection');
+
+// const app = express();
+// connectDb();
+// app.use(express.json());
+
+// app.use('/api/',allRoutes)
+
+
+// app.listen(process.env.PORT,()=>{
+//    console.log(`listening on port ${process.env.PORT}`);
+// });
+
+require('dotenv').config()
+const express = require('express')
+const app = express();
 
 const  allRoutes=require('./routes');
 const {connectDb}=require('./db/databaseconnection');
 
-const app = express();
+
 connectDb();
 app.use(express.json());
 
 app.use('/api/',allRoutes)
-
-
-app.listen(process.env.PORT,()=>{
-   console.log(`listening on port ${process.env.PORT}`);
-});
+app.listen(process.env.PORT, () => {
+    console.log(`server listening on port ${process.env.PORT}`)
+})
